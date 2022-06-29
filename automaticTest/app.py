a14ae8a7eda2258b8f4a0abf9ae4e324437c3595
franklin nwafor <chrysspoetry@gmail.com>
2022-06-29 22:22:56 +0200

    no more crawling

---
+++ automaticTest/app.py
@@ -0,0 +1,9 @@
+#automatic browsing => using selenim => one can automate browsing
+# from selenium import webdriver
+
+#creating an instance of chrome
+# browser = webdriver.Chrome()
+# browser.get("https://github.com")
+
+# sign_inLink= browser.find_element("Sign in")
+# sign_inLink.click()
